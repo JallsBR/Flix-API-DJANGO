03105a2a19d7b44a999797daa5897345c4836dc9
JallsBR <oswaldojales@gmail.com>
2025-04-08 16:38:25 -0300

    route genres list

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from genres.models import Genre
 
-# Create your views here.
+def genre_view(request):
+    genres = Genre.objects.all()
+    '''data = []
+    for genre in genres:
+        data.append({
+            'id': genre.id,
+            'name': genre.name 
+        }) 
+    '''       
+    #list compreension
+    data = [{'id': genre.id, 'name': genre.name} for genre in genres] 
+    return JsonResponse(data , safe=False)
+    
