69837613b1ff0cb6c474235000895ae8566390da
JallsBR <oswaldojales@gmail.com>
2025-04-09 09:57:08 -0300

    route genres detail, update

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-from genres.views import genre_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genres-list'),
+    path('admin/', admin.site.urls),    
+    path('genres/', genre_create_list_view, name='genres-create-list'),
+    path('genres/<int:pk>/', genre_detail_view , name='genres-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,16 +1,41 @@
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
-def genre_view(request):
-    genres = Genre.objects.all()
-    '''data = []
-    for genre in genres:
-        data.append({
-            'id': genre.id,
-            'name': genre.name 
-        }) 
-    '''       
-    #list compreension
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres] 
-    return JsonResponse(data , safe=False)
+@csrf_exempt
+def genre_create_list_view(request):
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+
+        '''data = []
+        for genre in genres:
+            data.append({
+                'id': genre.id,
+                'name': genre.name 
+            }) 
+        '''       
+        #list compreension
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres] 
+        return JsonResponse(data , safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
+
     
+    
+@csrf_exempt
+def genre_detail_view(request, pk):
+    # genre = Genre.objects.get(pk=pk)
+    genre = get_object_or_404(Genre, pk=pk)
+    if request.method == 'GET':
+        data = { 'id': genre.id, 'name': genre.name}
+        return JsonResponse(data, status=200)
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse({'id': genre.id, 'name': genre.name}, status=201)
