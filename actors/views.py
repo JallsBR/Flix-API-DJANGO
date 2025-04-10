1cc5e3258bb1a85021052d0a42033db8b942d53e
JallsBR <oswaldojales@gmail.com>
2025-04-10 13:44:47 -0300

    route actors model crud vieww

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from actors.models import Actor
+from rest_framework import generics
+from actors.serializer import ActorSerializer
 
-# Create your views here.
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    
+    
+class ActoreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 from genres.views import GenreRetrieveUpdateDestroyView, GenreCreateListView
+from actors.views import ActorCreateListView, ActoreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
+    
     path('genres/', GenreCreateListView.as_view(), name='genres-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view() , name='genres-detail-view'),
+    
+    path('actors/', ActorCreateListView.as_view(), name='actors-create-list'),
+    path('actors/<int:pk>/', ActoreRetrieveUpdateDestroyView.as_view() , name='actors-detail-view'),
 ]
