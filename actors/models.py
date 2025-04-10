4863837beffcc2b09c564bcd2039a17ac96bb7b3
JallsBR <oswaldojales@gmail.com>
2025-04-10 11:29:08 -0300

    model actors

---
+++ actors/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+NATIONALITY_CHOISES = (
+    ('BR', 'Brazilian'),
+    ('US', 'American'),
+    ('MX', 'Mexican'),
+    ('AR', 'Argentinian'),
+    ('CO', 'Colombian'),
+    ('VE', 'Venezuelan'),
+    ('EC', 'Ecuadorian'),
+    ('CL', 'Chilean')
+)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    nationality = models.CharField(null=True, blank=True, choices=NATIONALITY_CHOISES, max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.Name
+
